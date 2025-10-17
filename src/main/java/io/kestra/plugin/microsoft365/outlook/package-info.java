965f5120e06522bf6b45db6239d0ad1d45e06974
bishalbera <bishalbera2@gmail.com>
2025-10-17 18:09:54 +0530

    feat: introducing outlook plugin

---
+++ src/main/java/io/kestra/plugin/microsoft365/outlook/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Outlook",
+    description = "Outlook email integration for Microsoft 365. Send emails, list messages, retrieve specific emails, and trigger workflows on new incoming mail using Microsoft Graph API.",
+    categories = PluginSubGroup.PluginCategory.MESSAGING
+)
+package io.kestra.plugin.microsoft365.outlook;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
