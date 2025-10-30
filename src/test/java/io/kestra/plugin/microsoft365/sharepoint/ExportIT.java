3eef60e210e0bd27c2d16426f40bded834911644
bishalbera <bishalbera2@gmail.com>
2025-10-30 16:46:01 +0530

    fix: fixed export tests

--- src/test/java/io/kestra/plugin/microsoft365/sharepoint/ExportIT.java
+++ src/test/java/io/kestra/plugin/microsoft365/sharepoint/ExportIT.java
@@ -65,7 +65,7 @@ class ExportIT {
             .siteId(Property.ofValue(System.getenv("SHAREPOINT_SITE_ID")))
             .driveId(Property.ofValue(System.getenv("SHAREPOINT_DRIVE_ID")))
             .itemId(Property.ofValue(file.getItemId()))
-            .format(Property.ofValue(Export.FormatType.valueOf("HTML")))
+            .format(Property.ofValue(Export.FormatType.HTML))
             .build();
 
         Export.Output output = exportTask.run(runContext);
@@ -110,7 +110,7 @@ class ExportIT {
             .siteId(Property.ofValue(System.getenv("SHAREPOINT_SITE_ID")))
             .driveId(Property.ofValue(System.getenv("SHAREPOINT_DRIVE_ID")))
             .itemId(Property.ofValue(file.getItemId()))
-            .format(Property.ofValue(Export.FormatType.valueOf("PDF")))
+            .format(Property.ofValue(Export.FormatType.PDF))
             .build();
 
         Export.Output output = exportTask.run(runContext);
