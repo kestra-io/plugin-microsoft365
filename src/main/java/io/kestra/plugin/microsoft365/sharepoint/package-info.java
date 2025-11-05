b8467a0b7abca543c1e7996963cca9ec88e2b9db
François Delbrayelle <fdelbrayelle@gmail.com>
2025-11-05 11:51:12 +0100

    doc: add missing package-info.java for SharePoint

---
+++ src/main/java/io/kestra/plugin/microsoft365/sharepoint/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "SharePoint",
+    description = "SharePoint integration for Microsoft 365.",
+    categories = PluginSubGroup.PluginCategory.MESSAGING
+)
+package io.kestra.plugin.microsoft365.sharepoint;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
